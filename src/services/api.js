967bae7942a0a34d5d304ce5b0885ea29d35f009
jennifertakagi <jennitakagi@gmail.com>
2020-08-30 20:23:59 -0300

    chores: add logo, favicon, global css and api mock

---
+++ src/services/api.js
@@ -0,0 +1,36 @@
+const api = [
+  {
+    id: 0,
+    title: 'Data structures',
+    name: 'Jacqueline Cardozo',
+    jobTitle: 'Front-end Dev Leader',
+    duration: '45 min',
+    category: ['new', 'development'],
+  },
+  {
+    id: 1,
+    title: 'Tech to watch in 2020',
+    name: 'João Olivio',
+    jobTitle: 'Front-end Dev Leader',
+    duration: '1h 15 min',
+    category: ['new', 'development'],
+  },
+  {
+    id: 2,
+    title: 'How to get better at problem solving',
+    name: 'Derrick Pierce',
+    jobTitle: 'Front-end Dev Leader',
+    duration: '2h 35 min',
+    category: ['new', 'project management'],
+  },
+  {
+    id: 3,
+    title: 'How to keep your design consistent',
+    name: 'Thomas Cardoso',
+    jobTitle: 'Designer',
+    duration: '1h 50 min',
+    category: ['new', 'design'],
+  },
+];
+
+export default api;

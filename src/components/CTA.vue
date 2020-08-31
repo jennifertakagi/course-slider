212171752e68c8e0d394fe9e8a89f35fc46b8cec
jennifertakagi <jennitakagi@gmail.com>
2020-08-31 10:15:06 -0300

    feat: add cta component

---
+++ src/components/CTA.vue
@@ -0,0 +1,40 @@
+<template>
+  <a
+    tabindex="0"
+    role="button"
+    aria-pressed="false"
+    :aria-label="this.linkAriaLabel"
+    :title="this.linkTitle"
+    :href="this.linkURL"
+    id="button-view-more"
+  >
+    {{linkLabel}}
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'CTA',
+  data() {
+    return {
+      linkLabel: 'View More',
+      linkAriaLabel: 'Click to view more episodes',
+      linkTitle: 'Clicking on this button you will navigate to Valtech website',
+      linkURL: '//www.valtech.com/insights/',
+    };
+  },
+};
+</script>
+
+<style scoped>
+#button-view-more {
+  text-decoration: none;
+  color: var(--color-text-base);
+  width: 146px;
+  height: 50px;
+  padding-top: 10px;
+  margin: 3% auto;
+  text-align: center;
+  border: 2px solid var(--color-text-base);
+}
+</style>
